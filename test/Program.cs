1fc4cd2e5ade53af3a794783911770df3ab1057c
Thomas Boop <thboop@microsoft.com>
2019-11-11 13:56:37 -0500

    first commit

---
+++ test/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var command = "'" + Directory.GetCurrentDirectory().Replace(@"\\", @"\") + @"\test.ps1'""";
+            var _proc = new Process();
+            // REPLACE WITH DIRECTORY OF YOUR PWSH
+            _proc.StartInfo.FileName = @"C:\Program Files\PowerShell\6\pwsh.EXE";
+            var arguement = @"-command "". " + command;
+            _proc.StartInfo.Arguments = arguement;
+            _proc.StartInfo.UseShellExecute = false;
+            _proc.StartInfo.RedirectStandardInput = true;
+            _proc.StartInfo.RedirectStandardError = true;
+            _proc.StartInfo.RedirectStandardOutput = true;
+            
+            // ******************************************************************
+            // Setting this line to true causes powershell core to exit code 1
+            _proc.StartInfo.CreateNoWindow = true; 
+            // ******************************************************************
+
+            var response = _proc.Start();
+            _proc.WaitForExit();
+            Console.WriteLine(_proc.StandardOutput.ReadToEnd());
+            Console.WriteLine(_proc.StandardError.ReadToEnd());
+            Console.WriteLine($"exited with exit code {_proc.ExitCode}");
+        }
+    }
+}
---
+++ test/test.ps1
@@ -0,0 +1,5 @@
+$ErrorActionPreference = 'stop'
+
+npm ping
+npm ping
+npm ping
